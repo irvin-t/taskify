a7427468dbbb1098f57797f85ffe24c33f51647e
Irvin <irvintermulo@gmail.com>
2024-02-12 17:06:04 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,3 +13,17 @@ root.render(
 );
 
 
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+
